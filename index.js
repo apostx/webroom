e4e51fa294b79377bc42a95f41af24fc99c9d2a5
Richard A. Sallai <apostx@users.noreply.github.com>
2020-02-14 23:06:25 +0100

    ModuleUpdater got its own repo (apostx/module-updater-js) and changed repo urls

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 const WebRoom = {};
 
-WebRoom.ModuleUpdater = require('./lib/module-updater');
 WebRoom.AbstractWebRoom = require('./lib/abstract-webroom');
 WebRoom.Server = require('./lib/webroom-server');
 WebRoom.SocketUnifiedAdapter = require('./lib/socket-unifiedadapter');
